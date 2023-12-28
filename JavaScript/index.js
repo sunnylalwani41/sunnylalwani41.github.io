b4225f4b0bf45dc0f5a5300c2f5e7f71533e9b6c
Sunny Lalwani <sunnylalwani41@gmail.com>
2023-12-28 17:12:19 +0530

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,31 +5,3 @@ let typingEffect=new Typed(".multiText", {
     backSpeed : 80,
     backDelay : 1500
 })
-GitHubCalendar(".calendar", "sunnylalwani41");
-
-// or enable responsive functionality:
-GitHubCalendar(".calendar", "sunnylalwani41", { responsive: true });
-
-// Use a proxy
-fetch('https://api.github.com/users/<username>/contributions', {
-    headers: {
-        'Authorization': 'token <your_GitHub_token>'
-    }
-})
-.then(response => response.json()).then(r => r.text())
-
-burger=document.querySelector(".burger");
-navbar=document.querySelector(".navbar");
-navList=document.querySelector(".nav-list");
-rightNav=document.querySelector(".rightNav");
-burger.addEventListener("click", ()=>{
-    navbar.classList.toggle("h-nav");
-    navList.classList.toggle("v-nav");
-    rightNav.classList.toggle("v-nav");
-    
-})
-
-
-.then(data => {
-    // Process the contribution data
-});
