5ffc66acc158c3dc61bdf8fbbf1fce174f75b4ba
sunnylalwani41 <sunnylalwani41@gmail.com>
2025-10-24 21:32:16 +0530

    new portfolio

--- routes/phonepe/phonepeRoute.js
+++
@@ -1,8 +0,0 @@
-const {newPayment, checkStatus} = require('../../controller/phonepe/paymentController');
-const express = require('express');
-const router = express();
- 
-router.post('/payment', newPayment);
-router.post('/status/:txnId', checkStatus);
-
-module.exports = router;
